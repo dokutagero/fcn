53afa97094c53b9abe2bd8dac729cf85d1412c47
Unknown <jonas.hepp@tum.de>
2018-01-10 17:46:15 +0900

    added data augmentation

--- fcn/datasets/bridge.py
+++ fcn/datasets/bridge.py
@@ -5,6 +5,9 @@ import os.path as osp
 import piexif
 import scipy.io
 
+import imgaug as ia
+from imgaug import augmenters as iaa
+
 from PIL import Image
 from .. import data
 
@@ -16,20 +19,44 @@ class BridgeSegBase(chainer.dataset.DatasetMixin):
     class_names = np.array(['non-damage', 'delamination', 'rebar_exposure'])
     class_weight_default = np.array([0.3610441, 4.6313269, 69.76223605]) #the weights will be multiplied with the loss value
 
-    def __init__(self, split='train', black_out_non_deck=False, use_class_weight=False):
+    def __init__(self, split='train', use_data_augmentation=False, black_out_non_deck=False, use_class_weight=False):
         self.split = split
         self.black_out_non_deck = black_out_non_deck
-        if black_out_non_deck:
-            self.class_names = np.array(['non-damage', 'delamination', 'rebar_exposure', 'non-deck'])
-
-        self.files = collections.defaultdict(list)
+        self.use_data_augmentation = use_data_augmentation
+        #if black_out_non_deck or use_data_augmentation:
+            #see below with the class weights
+            #self.class_names = np.array(['non-damage', 'delamination', 'rebar_exposure', 'non-deck'])
+        
         if use_class_weight:
-            self.class_weight = class_weight_default
-            if black_out_non_deck:
-                self.class_weight = np.append(self.class_weight, 0.0)
+            self.class_weight = BridgeSegBase.class_weight_default
+            #if black_out_non_deck:
+                #non deck is label -1, should be ignored by the softmax cross entropy
+                #so adding an additional weight might fuck up the order if classes
+                #lets see if this throws an error or not
+                #self.class_weight = np.append(self.class_weight, 0.0)
         else:
             self.class_weight = None
 
+        if self.use_data_augmentation:
+            ia.seed(42)
+            self.seq = iaa.Sequential([
+                iaa.Fliplr(0.5, name='Fliplr'),
+                iaa.Flipud(0.5, name='Flipud'),
+                iaa.Sometimes(0.5, iaa.Affine(scale={"x": (0.8, 1.2), "y": (0.8, 1.2)}, rotate=(-45, 45), shear=(-15, 15), name='Affine')),
+                iaa.SomeOf((0,1), [ #pick from 0 up to 1 out of the following to apply
+                    iaa.ContrastNormalization((0.75, 1.5), name='ContrastNormalization'),     
+                    iaa.Multiply((0.8, 1.2), name='Multiply')
+                ], random_order=True)
+            ])
+            def activator_lbl(images, augmenter, parents, default):
+                if augmenter.name in ['ContrastNormalization', 'Multiply']:  #all these will not be performed on the labels
+                    return False
+                else:
+                    # default value for all other augmenters
+                    return default
+            self.hooks_lbl = ia.HooksImages(activator=activator_lbl)
+
+        self.files = collections.defaultdict(list)
         for split in ['train', 'validation', 'all']:
             imgsets_file = osp.join(DATASET_BRIDGE_DIR, "{}.txt".format(split))
             for did in open(imgsets_file):
@@ -73,14 +100,24 @@ class BridgeSegBase(chainer.dataset.DatasetMixin):
 
         if self.rcrop.any() != None:
             img,lbl = self.random_crop(img,lbl, self.rcrop)
+
+        if self.use_data_augmentation:
+            lbl+=1 #imaug library pads with 0. We want the label to be padded with 'non-deck', which has the label -1, hence this cheap hack
+            img, lbl = self.augment_image(img,lbl)
+            lbl-=1
+            #if np.unique(lbl).shape[0] > len(self.class_names):
+            if np.unique(lbl).shape[0] > len(self.class_names)+1: #+1 because we add -1 as a label, whcih doesnt have a class name
+                print('WARNING: someting is odd about the number of labeled classes in this image, the are {} (label: {})'.format(np.unique(lbl), lbl_file))
+
+
         return img, lbl
 
     def random_crop(self, x, y=np.array([None]), random_crop_size=None):
         w, h = x.shape[1], x.shape[0]
         rangew = (w - random_crop_size[0]) // 2
         rangeh = (h - random_crop_size[1]) // 2
-        offsetw = 0 if rangew==0 else np.random.randint(rangew)
-        offseth = 0 if rangeh==0 else np.random.randint(rangeh)
+        offsetw = 0 if rangew<=0 else np.random.randint(rangew)
+        offseth = 0 if rangeh<=0 else np.random.randint(rangeh)
         if y.any() != None:
             return x[offseth:offseth+random_crop_size[0], offsetw:offsetw+random_crop_size[1]], \
                     y[offseth:offseth+random_crop_size[0], offsetw:offsetw+random_crop_size[1]]
@@ -116,11 +153,20 @@ class BridgeSegBase(chainer.dataset.DatasetMixin):
         img = img * deck.astype('uint8') 
         return img, lbl
 
+    def augment_image(self, img, lbl):
+    
+        seq_det = self.seq.to_deterministic() #seq_det is now a fixes sequence, so lbl and deck get treated the same as img
+                                         #it will be a new one after each call of seq.to_deterministic()  
+        img = seq_det.augment_image(img)
+        lbl = seq_det.augment_image(lbl, hooks=self.hooks_lbl)
+
+        return img, lbl
+
         
 
 class BridgeSeg(BridgeSegBase):
-    def __init__(self, split='train', rcrop=[None, None], black_out_non_deck=False, use_class_weight=False):
-       super(BridgeSeg, self).__init__(split=split, black_out_non_deck=black_out_non_deck, use_class_weight=use_class_weight) 
+    def __init__(self, split='train', rcrop=[None, None], use_data_augmentation=False, black_out_non_deck=False, use_class_weight=False):
+       super(BridgeSeg, self).__init__(split=split, use_data_augmentation=use_data_augmentation, black_out_non_deck=black_out_non_deck, use_class_weight=use_class_weight) 
        if len(rcrop) == 2:
            self.rcrop = np.array(rcrop)
        else:
