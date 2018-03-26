54028db486ae9911ab40cc3f648775ac31430f31
mrteera <inbox@mrteera.com>
2018-03-26 16:28:48 +0700

    Deal with rebar_exposure inside delamination

--- scripts/calc_blob_size_hist.py
+++ scripts/calc_blob_size_hist.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import xmltodict
 
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+
 
 def get_xml(filename):
     with open(filename) as fd:
@@ -21,7 +24,11 @@ def remove_deleted_mask(doc):
 
 
 # https://stackoverflow.com/a/24468019/1971997
-def PolygonArea(corners):
+# TODO: need to calculate polygon area with holes
+def PolygonArea(damage_pt):
+    corners = []
+    for pt in damage_pt:
+        corners.append((int(pt['x']), int(pt['y'])))
     n = len(corners) # of corners
     area = 0.0
     for i in range(n):
@@ -29,7 +36,7 @@ def PolygonArea(corners):
         area += corners[i][0] * corners[j][1]
         area -= corners[j][0] * corners[i][1]
     area = abs(area) / 2.0
-    return area
+    return [area, corners]
 
 
 if __name__ == '__main__':
@@ -39,15 +46,27 @@ if __name__ == '__main__':
     rebar_exposure_blob_size = []
 
     for damage in damage_list:
-        corners = []
-        if damage['name'] in ['delamination', 'rebar_exposure']:
-            for pt in damage['polygon']['pt']:
-              corners.append((int(pt['x']), int(pt['y'])))
-            blob_size = PolygonArea(corners)
-            if damage['name'] == 'delamination':
-                delamination_blob_size.append(blob_size)
-            if damage['name'] == 'rebar_exposure':
-                rebar_exposure_blob_size.append(blob_size)
+        delamination_corners = []
+        rebar_exposure_corners = []
+        if damage['name'] == 'delamination':
+            [blob_size, delamination_corners] = PolygonArea(damage['polygon']['pt'])
+            delamination_poly = Polygon(delamination_corners)
+            for damage in damage_list:
+                if damage['name'] == 'rebar_exposure':
+                    has_rebar_in_delamination = []
+                    for pt in damage['polygon']['pt']:
+                        point = Point(int(pt['x']), int(pt['y']))
+                        has_rebar_in_delamination.append(point.within(delamination_poly))
+                    if all(has_rebar_in_delamination) == True:
+                        blob_size = blob_size - PolygonArea(damage['polygon']['pt'])[0]
+                    elif all(has_rebar_in_delamination) == False:
+                        pass
+                    else:
+                        print('Something wrong with rebar_exposure')
+            delamination_blob_size.append(blob_size)
+        elif damage['name'] == 'rebar_exposure':
+            blob_size = PolygonArea(damage['polygon']['pt'])[0]
+            rebar_exposure_blob_size.append(blob_size)
     plt.figure(1)
     plt.hist(delamination_blob_size, bins=100)
     plt.title('Histogram of delamination blob size')
