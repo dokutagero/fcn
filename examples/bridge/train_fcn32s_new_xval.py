63eb4082006d3b2c63b71ff0eecd37d816b74f42
dokutagero <kendixd@gmail.com>
2018-03-30 11:14:02 +0900

    file for xvalidation with new trainer

---
+++ examples/bridge/train_fcn32s_new_xval.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+
+import argparse
+import datetime
+import os
+import os.path as osp
+
+os.environ['MPLBACKEND'] = 'Agg'  # NOQA
+
+import chainer
+from chainer.training import extensions
+import chainercv
+import fcn
+
+
+here = osp.dirname(osp.abspath(__file__))
+
+
+def get_data(deck_flag, data_augmentation, tstrategy):
+    dataset_train = fcn.datasets.BridgeSeg(split='train')
+
+    class_names = dataset_train.class_names
+
+    # Include this parameters in main function
+    deck_flag = bool(deck_flag) 
+    data_augmentation = bool(data_augmentation)
+    class_weight_flag = False
+
+
+
+    dataset_train = datasets.BridgeSeg(
+        split='trainval',
+        tstrategy=tstrategy,
+        rcrop=[256,256],
+        use_class_weight=class_weight_flag,
+        black_out_non_deck=deck_flag,
+        use_data_augmentation=data_augmentation
+    )
+    
+    dataset_nocrop = datasets.BridgeSeg(
+        split='trainval',
+        tstrategy=2,
+        use_class_weight=class_weight_flag,
+        black_out_non_deck=deck_flag,
+        use_data_augmentation=False
+    )
+
+    if xval>0:
+        dataset_cv = chainer.datasets.get_cross_validation_datasets_random(dataset_train, 5, 42)
+        dataset_nocrop_cv = chainer.datasets.get_cross_validation_datasets_random(dataset_nocrop, 5, 42)
+
+    
+
+#     dataset_valid = datasets.BridgeSeg(
+#         split='validation',
+#         use_class_weight=class_weight_flag,
+#         black_out_non_deck=deck_flag,
+#         use_data_augmentation=False
+#     )
+
+    # Apply per channel mean substraction
+    dataset_train = chainer.datasets.TransformDataset(
+        dataset_train, fcn.datasets.transform_lsvrc2012_vgg16)
+#     dataset_valid = chainer.datasets.TransformDataset(
+#         dataset_valid, fcn.datasets.transform_lsvrc2012_vgg16)
+    dataset_nocrop = chainer.datasets.TransformDataset(
+        dataset_nocrop, fcn.datasets.transform_lsvrc2012_vgg16)
+    num_train_samples = len(dataset_train)
+    
+    # Create iterators
+#     iter_train = chainer.iterators.SerialIterator(
+#         dataset_train, batch_size=1)
+#     iter_valid = chainer.iterators.SerialIterator(
+#         dataset_valid, batch_size=1, repeat=False, shuffle=False)
+#     iter_train_nocrop = chainer.iterators.SerialIterator(
+#         dataset_train_nocrop, batch_size=1, repeat=False, shuffle=False)
+
+    #return num_train_samples, class_names, iter_train, iter_valid, iter_train_nocrop
+    return num_train_samples, class_names, dataset_train, dataset_cv, dataset_nocrop 
+
+
+def get_trainer(optimizer, iter_train, iter_valid, iter_train_nocrop,
+                class_names, args):
+    model = optimizer.target
+
+    updater = chainer.training.StandardUpdater(
+        iter_train, optimizer, device=args.gpu)
+
+    trainer = chainer.training.Trainer(
+        updater, (args.max_iteration, 'iteration'), out=args.out)
+
+    trainer.extend(fcn.extensions.ParamsReport(args.__dict__))
+
+    trainer.extend(extensions.ProgressBar(update_interval=5))
+
+    trainer.extend(extensions.LogReport(
+        trigger=(args.interval_print, 'iteration')))
+    trainer.extend(extensions.PrintReport(
+        ['epoch', 'iteration', 'elapsed_time',
+         'main/loss', 'validation/main/miou']))
+
+    def pred_func(x):
+        model(x)
+        return model.score
+
+    # trainer.extend(
+    #     fcn.extensions.SemanticSegmentationVisReport(
+    #         pred_func, iter_valid_raw,
+    #         transform=fcn.datasets.transform_lsvrc2012_vgg16,
+    #         class_names=class_names, device=args.gpu, shape=(4, 2)),
+    #     trigger=(args.interval_eval, 'iteration'))
+
+    trainer.extend(
+        chainercv.extensions.SemanticSegmentationEvaluator(
+            iter_valid, model, label_names=class_names),
+        trigger=(args.interval_eval, 'iteration'))
+
+    trainer.extend(
+        chainercv.extensions.SemanticSegmentationEvaluator(
+            iter_train_nocrop, model, label_names=class_names),
+        trigger=(args.interval_eval, 'iteration'))
+
+    trainer.extend(extensions.snapshot_object(
+        target=model, filename='model_best.npz'),
+        trigger=chainer.training.triggers.MaxValueTrigger(
+            key='validation/main/miou',
+            trigger=(args.interval_eval, 'iteration')))
+
+    assert extensions.PlotReport.available()
+    trainer.extend(extensions.PlotReport(
+        y_keys=['main/loss'], x_key='iteration',
+        file_name='loss.png', trigger=(args.interval_print, 'iteration')))
+    trainer.extend(extensions.PlotReport(
+        y_keys=['validation/main/miou'], x_key='iteration',
+        file_name='miou.png', trigger=(args.interval_print, 'iteration')))
+
+    return trainer
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-g', '--gpu', type=int, required=True, help='GPU id')
+    parser.add_argument('-da', '--data-augmentation', type=int, \
+                        default=0, choices=(0,1),
+                        help='Data augmentation flag. Default 0, 1 for data augmentation')
+    parser.add_argument('-d', '--deck-mask', type=int, default=1, choices=(0,1),\
+                        help='Applying deck mask. Default 1, 0 for not masking deck')
+    parser.add_argument('-e', '--epochs', type=int, default=100, choices=range(1000), \
+                        help='Number of epochs', metavar='range(0...1000)')
+    parser.add_argument('-x', '--xval', type=int, default=5)
+    parser.add_argument('-t', '--tstrategy', type=int, default=0, choices=(0,1)
+    args = parser.parse_args()
+
+    args.model = 'FCN32s'
+    args.lr = 1e-10
+    args.momentum = 0.99
+    args.weight_decay = 0.0005
+
+    args.interval_print = 20
+
+    #tstrategy is train strategy, 0 for random sample of label and 1 for intersection
+    # hardcoded value of 2 is for evaluation.
+
+    # data
+#     num_train_samples, class_names, iter_train, iter_valid, iter_train_nocrop = get_data(args.deck_mask, \
+#                                                                       args.data_augmentation)
+# 
+    num_train_samples, class_names, dataset_train, dataset_cv, dataset_nocrop = get_data(args.deck_mask, \
+                                                                      args.data_augmentation)
+    for fold, (train_fold, valid_fold) in enumerate(dataset_cv):
+        now = datetime.datetime.now()
+        args.timestamp = now.isoformat()
+        args.out = osp.join(here, 'logs', now.strftime('%Y%m%d_%H%M%S'), '_'+str(fold))
+
+        n_class = len(class_names)
+        args.max_iteration = args.epochs * int((train_samples)*((args.xval-1) * args.xval))
+        args.interval_eval = int((train_samples)*((args.xval-1) * args.xval))
+
+        (train_fold_nocrop, test_fold_nocrop) = dataset_nocrop[fold]
+        iter_train = chainer.iterators.SerialIterator(
+            train_fold, batch_size=1)
+        iter_valid = chainer.iterators.SerialIterator(
+            valid_fold, batch_size=1, repeat=False, shuffle=False)
+        iter_train_nocrop = chainer.iterators.SerialIterator(
+            dataset_train_nocrop, batch_size=1, repeat=False, shuffle=False)
+
+        # model
+        vgg = fcn.models.VGG16()
+        chainer.serializers.load_npz(vgg.pretrained_model, vgg)
+        model = fcn.models.FCN32s(n_class=n_class)
+        model.init_from_vgg16(vgg)
+
+        if args.gpu >= 0:
+            chainer.cuda.get_device(args.gpu).use()
+            model.to_gpu()
+
+        # optimizer
+        optimizer = chainer.optimizers.MomentumSGD(
+            lr=args.lr, momentum=args.momentum)
+        optimizer.setup(model)
+        optimizer.add_hook(chainer.optimizer.WeightDecay(rate=args.weight_decay))
+        for p in model.params():
+            if p.name == 'b':
+                p.update_rule = chainer.optimizers.momentum_sgd.MomentumSGDRule(
+                    lr=optimizer.lr * 2, momentum=0)
+        model.upscore.disable_update()
+
+        # trainer
+        trainer = get_trainer(optimizer, iter_train, iter_valid, iter_train_nocrop,
+                              class_names, args)
+        trainer.run()
+
+
+if __name__ == '__main__':
+    main()
